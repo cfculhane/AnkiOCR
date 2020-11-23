1344ff3673c5c2b2ca927def88470fea65ac971a
Chris Culhane <cfculhane@gmail.com>
2020-11-23 22:26:09 +1100

    Finalising multithreading analysis - deciding to use ThreadPoolExecutor with batches of images
    NUM PROCESSES/THREADS : 3
    BATCH_SIZE : 10
    Number of images = 200
    'gen_batched_txts' completed in 0.008 s
    Generated 20 batches of max 10 images
    'seq_iter' completed in 170.1169 s
    'big_batch' completed in 132.7926 s
    'mp_pool' completed in 63.4509 s
    'mp_pool_batched' completed in 57.1407 s
    'thread_pool_exec' completed in 48.9472 s
    'process_pool_exec' completed in 41.051 s
    'process_pool_exec_batched' completed in 38.7359 s
    'thread_pool_exec_batched' completed in 38.5955 s

--- tests/multithreading_timing.py
+++ tests/multithreading_timing.py
@@ -1,19 +1,19 @@
 # Some basic tests to make sure major breaking changes dont occur
 import itertools
+import tempfile
 import time
 from concurrent.futures.process import ProcessPoolExecutor
 from concurrent.futures.thread import ThreadPoolExecutor
 from multiprocessing import Pool
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, List, Tuple
 
 import pytesseract
 from tqdm import tqdm
 
-IMGS_DIR = Path(__file__).parent / "batch_imgs"
-IMG_PTHS = [str(img_pth) for img_pth in IMGS_DIR.glob("*.png")]
-TXT_PATH = Path(IMGS_DIR, "imgs.txt")
-TXT_PATH.write_text("\n".join([str(i) for i in IMG_PTHS]))
+IMGS_DIR = Path(__file__).parent / "testdata" / "batch_imgs"
+assert IMGS_DIR.exists()
+
 tesseract_cmd = str(Path(r"C:\GitHub\AnkiOCR\anki_ocr\deps\win\tesseract\tesseract.exe"))
 pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
 NUM_PROCESSES = 4
@@ -53,14 +53,15 @@ def batch(it: Iterable, batch_size):
 
 
 @timeit
-def gen_batched_txts(img_pths, batch_size):
+def gen_batched_txts(img_pths: List[Path], batch_size: int) -> Tuple[List[Path], tempfile.TemporaryDirectory]:
+    batched_txts_dir = tempfile.TemporaryDirectory()  # Need to return so we can cleanup later
     batched_txts = []
     for i, batched_img_pths in enumerate(batch(img_pths, batch_size)):
-        batch_txt_pth = Path(IMGS_DIR, f"batch_imgs_{i}.txt")
+        batch_txt_pth = Path(batched_txts_dir.name, f"batch_imgs_{i}.txt")
         batch_txt_pth.write_text("\n".join([str(i) for i in batched_img_pths]))
         batched_txts.append(batch_txt_pth)
 
-    return batched_txts
+    return batched_txts, batched_txts_dir
 
 
 def tesseract(img_pth):
@@ -84,7 +85,7 @@ def time_pool_progress(thread_f, thread_iter):
     with Pool(processes=NUM_PROCESSES) as pool:
         # print "[0, 1, 4,..., 81]"
         # pooled__batch_output = pool.map(f, batched_txts_)
-        max_ = len(batched_txts_)
+        max_ = len(thread_iter)
         with tqdm(total=max_) as pbar:
             for i, _ in enumerate(pool.imap_unordered(thread_f, thread_iter)):
                 pbar.update()
@@ -98,8 +99,30 @@ def thread_pool_exec(thread_f, thread_iter):
 
 @timeit
 def thread_pool_exec_batched(thread_f, thread_iter):
+    results = []
     with ThreadPoolExecutor() as executor:
-        executor.map(thread_f, thread_iter)
+        print(f"Using {executor._max_workers} threads")
+        max_ = len(thread_iter)
+        with tqdm(total=max_, unit="batch") as pbar:
+            for i, output in enumerate(executor.map(thread_f, thread_iter)):
+                results.append(output)
+                pbar.update()
+    return results
+
+
+def process_result(future):
+    print(future.result())
+
+
+def run_proc(thread_f, thread_iter):
+    pbar = tqdm(total=len(thread_iter), unit="batch")
+    executor = ThreadPoolExecutor()
+    futures = [executor.submit(thread_f, i) for i in thread_iter]
+    for future in futures:
+        future.add_done_callback(process_result)
+    executor.shutdown()
+    print("done")
+    return futures
 
 
 @timeit
@@ -110,25 +133,34 @@ def process_pool_exec(thread_f, thread_iter):
 
 @timeit
 def process_pool_exec_batched(thread_f, thread_iter):
+    results = []
     with ProcessPoolExecutor() as executor:
-        executor.map(thread_f, thread_iter)
+        max_ = len(thread_iter)
+        with tqdm(total=max_, unit="batch") as pbar:
+            for i, output in enumerate(executor.map(thread_f, thread_iter)):
+                results.append(output)
+                pbar.update()
+    return results
 
 
 if __name__ == '__main__':
+    IMG_PTHS = [img_pth.absolute() for img_pth in IMGS_DIR.glob("*.png")]
+    TXT_PATH = Path(IMGS_DIR, "imgs.txt")
+    TXT_PATH.write_text("\n".join([str(i) for i in IMG_PTHS]))
+
     print(f"NUM PROCESSES/THREADS : {NUM_PROCESSES}")
     print(f"BATCH_SIZE : {BATCH_SIZE}")
     print(f"Number of images = {len(IMG_PTHS)}")
-    batched_txts_ = gen_batched_txts(IMG_PTHS, BATCH_SIZE)
-    print(f"Generated {len(batched_txts_)} batches of max {BATCH_SIZE} images")
+    batched_txts, batched_txts_dir = gen_batched_txts(IMG_PTHS, BATCH_SIZE)
+    print(f"Generated {len(batched_txts)} batches of max {BATCH_SIZE} images")
 
     seq_iter()
     big_batch()
     mp_pool(tesseract, IMG_PTHS)
-    mp_pool_batched(tesseract, batched_txts_)
+    mp_pool_batched(tesseract, batched_txts)
     thread_pool_exec(tesseract, IMG_PTHS)
-    thread_pool_exec_batched(tesseract, batched_txts_)
-    process_pool_exec(tesseract, batched_txts_)
-    process_pool_exec_batched(tesseract, batched_txts_)
-    for pth in batched_txts_:
-        pth.unlink(missing_ok=True)
-    print("Removed batch txt files")
+    thread_pool_exec_batched(tesseract, batched_txts)
+    process_pool_exec(tesseract, batched_txts)
+    results = thread_pool_exec_batched(tesseract, batched_txts)
+
+    batched_txts_dir.cleanup()
