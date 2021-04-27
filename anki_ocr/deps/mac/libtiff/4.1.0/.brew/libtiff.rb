232ea5a9fecd8f29ac20de6f709a4c4df4ed35f5
chris360KS <cfculhane@gmail.com>
2021-04-27 16:37:03 +1000

    Adding standalone tesseract for mac, seperate out tessdata directory

---
+++ anki_ocr/deps/mac/libtiff/4.1.0/.brew/libtiff.rb
@@ -0,0 +1,39 @@
+class Libtiff < Formula
+  desc "TIFF library and utilities"
+  homepage "https://libtiff.gitlab.io/libtiff/"
+  url "https://download.osgeo.org/libtiff/tiff-4.1.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/tiff-4.1.0.tar.gz"
+  sha256 "5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634"
+
+  depends_on "jpeg"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-lzma
+      --with-jpeg-include-dir=#{Formula["jpeg"].opt_include}
+      --with-jpeg-lib-dir=#{Formula["jpeg"].opt_lib}
+      --without-x
+    ]
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <tiffio.h>
+
+      int main(int argc, char* argv[])
+      {
+        TIFF *out = TIFFOpen(argv[1], "w");
+        TIFFSetField(out, TIFFTAG_IMAGEWIDTH, (uint32) 10);
+        TIFFClose(out);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-ltiff", "-o", "test"
+    system "./test", "test.tif"
+    assert_match(/ImageWidth.*10/, shell_output("#{bin}/tiffdump test.tif"))
+  end
+end
