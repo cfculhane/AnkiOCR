b9b93a9df8038b552492edcd4945a5ca46edabcb
Chris Culhane <cfculhane@gmail.com>
2021-09-19 11:40:50 +1000

    Update create_standalone_tesseract_mac.sh

--- scripts/create_standalone_tesseract_mac.sh
+++ scripts/create_standalone_tesseract_mac.sh
@@ -23,19 +23,41 @@ cp -r /usr/local/Cellar/jpeg tesseract_standalone/
 cp -r /usr/local/Cellar/giflib tesseract_standalone/
 
 cd tesseract_standalone/tesseract/4.1.1/bin/
-install_name_tool -change /usr/local/opt/leptonica/lib/liblept.5.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib tesseract
-install_name_tool -change /usr/local/Cellar/tesseract/4.1.1/lib/libtesseract.4.dylib @executable_path/../lib/libtesseract.4.dylib tesseract
-install_name_tool -change /usr/local/opt/leptonica/lib/liblept.5.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib ../../../tesseract/4.1.1/lib/libtesseract.4.dylib
-install_name_tool -change /usr/local/opt/libpng/lib/libpng16.16.dylib ../../../libpng/1.6.37/lib/libpng16.16.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib
-install_name_tool -change /usr/local/opt/jpeg/lib/libjpeg.9.dylib ../../../jpeg/9d/lib/libjpeg.9.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib
-install_name_tool -change /usr/local/opt/giflib/lib/libgif.7.dylib ../../../giflib/5.2.1/lib/libgif.7.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib
-install_name_tool -change /usr/local/opt/giflib/lib/libgif.dylib ../../../giflib/5.2.1/lib/libgif.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib 
-install_name_tool -change /usr/local/opt/libtiff/lib/libtiff.5.dylib ../../../libtiff/4.3.0/lib/libtiff.5.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib
 
-# WebP
-install_name_tool -change /usr/local/opt/webp/lib/libwebp.7.dylib ../../../webp/1.2.1/lib/libwebp.7.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib
-install_name_tool -change /usr/local/opt/webp/lib/libwebpmux.3.dylib ../../../webp/1.2.1/lib/libwebpmux.3.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib
+GIFLIB_VERSION="5.2.1"
+LEPTONICA_VERSION="1.81.1"
+LIBPNG_VERSION="1.6.37"
+LIBTIFF_VERSION="4.3.0"
+LITTLE_CMS_VERSION="2.12"
+OPENJPEG_VERSION="2.4.0"
+TESSERACT_VERSION="4.1.1"
+WEBP_VERSION="1.2.1"
 
-# Openjpeg
-install_name_tool -change /usr/local/opt/openjpeg/lib/libopenjp2.7.dylib ../../../openjpeg/2.4.0/lib/libopenjp2.2.4.0.dylib ../../../leptonica/1.81.1/lib/liblept.5.dylib
-install_name_tool -change /usr/local/opt/jpeg/lib/libjpeg.9.dylib ../../../jpeg/9d/lib/libjpeg.9.dylib ../../../libtiff/4.3.0/lib/libtiff.5.dylib
+
+
+
+
+# Modifying libtesseract.4.dylib
+install_name_tool -change "/usr/local/Cellar/tesseract/${TESSERACT_VERSION}/lib/libtesseract.4.dylib" "@executable_path/../lib/libtesseract.4.dylib" tesseract
+install_name_tool -change "/usr/local/opt/leptonica/lib/liblept.5.dylib" "../../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib" "../../../tesseract/${TESSERACT_VERSION}/lib/libtesseract.4.dylib"
+
+# Modifying tesseract
+install_name_tool -change "/usr/local/opt/leptonica/lib/liblept.5.dylib" "../../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib" tesseract
+
+# Modifying liblept.5.dylib
+install_name_tool -change "/usr/local/opt/libpng/lib/libpng16.16.dylib" "../../../libpng/${LIBPNG_VERSION}/lib/libpng16.16.dylib" "../../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib"
+
+install_name_tool -change "/usr/local/opt/jpeg/lib/libjpeg.9.dylib" "../../../jpeg/9d/lib/libjpeg.9.dylib" ."./../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib"
+
+install_name_tool -change "/usr/local/opt/giflib/lib/libgif.7.dylib" "../../../giflib/${GIFLIB_VERSION}/lib/libgif.7.dylib" "../../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib"
+install_name_tool -change "/usr/local/opt/giflib/lib/libgif.dylib" "../../../giflib/${GIFLIB_VERSION}/lib/libgif.dylib" ."./../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib"
+
+install_name_tool -change "/usr/local/opt/libtiff/lib/libtiff.5.dylib" "../../../libtiff/${LIBTIFF_VERSION}/lib/libtiff.5.dylib" "../../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib"
+
+install_name_tool -change "/usr/local/opt/webp/lib/libwebp.7.dylib" "../../../webp/${WEBP_VERSION}/lib/libwebp.7.dylib" "../../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib"
+install_name_tool -change "/usr/local/opt/webp/lib/libwebpmux.3.dylib" "../../../webp/${WEBP_VERSION}/lib/libwebpmux.3.dylib" "../../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib"
+
+install_name_tool -change "/usr/local/opt/openjpeg/lib/libopenjp2.7.dylib" "../../../openjpeg/${OPENJPEG_VERSION}/lib/libopenjp2.2.4.0.dylib" "../../../leptonica/${LEPTONICA_VERSION}/lib/liblept.5.dylib"
+
+# Modifying libtiff
+install_name_tool -change "/usr/local/opt/jpeg/lib/libjpeg.9.dylib" "../../../jpeg/9d/lib/libjpeg.9.dylib" "../../../libtiff/${LIBTIFF_VERSION}/lib/libtiff.5.dylib"
