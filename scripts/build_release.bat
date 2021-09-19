53e814a8fcd3117c2afe9cd691056cfb2a650610
Chris Culhane <cfculhane@gmail.com>
2021-09-19 15:35:40 +1000

    Adding manifest file

--- scripts/build_release.bat
+++ scripts/build_release.bat
@@ -4,8 +4,8 @@ echo Copying source code to dist folder ...
 cd ..
 robocopy .\anki_ocr .\dist\anki_ocr /E /PURGE /XD "__pycache__" "logs" /XF *.pickle *.pyc *.sqlite meta.json
 cd .\dist\anki_ocr
-IF EXIST "..\anki_ocr.zip" (
-    del /f  "..\anki_ocr.zip"
+IF EXIST "..\anki_ocr.ankiaddon" (
+    del /f  "..\anki_ocr.ankiaddon"
 )
 7z a "../anki_ocr.zip" .
 cd ..
